4cad9ea4acb2cc5cad9a30f98e6d16e22c65132c
RashLop <lopezmejiarasshid@outlook.com>
2025-10-27 17:03:01 -0400

    feat: dbContext added

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -16,22 +16,55 @@ namespace TecWebFest.Api.Data
         {
             base.OnModelCreating(modelBuilder);
             //TODO
+            modelBuilder.Entity<Artist>(a =>
+            {
+                a.HasKey(a => a.Id);
+                a.Property(a => a.StageName).IsRequired().HasMaxLength(100);
+                a.Property(a => a.Genre).IsRequired().HasMaxLength(50);
+                a.HasMany(a => a.Performances)
+                  .WithOne(p => p.Artist)
+                  .HasForeignKey(s => s.ArtistId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+            });
+            modelBuilder.Entity<Stage>(s =>
+            {
+                s.HasKey(s => s.Id);
+                s.Property(s => s.Name).IsRequired().HasMaxLength(100);
+                s.Property(s => s.FestivalId).IsRequired();
+                s.HasMany(s => s.Performances)
+                  .WithOne(p => p.Stage)
+                  .HasForeignKey(p => p.StageId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            }); 
 
             // 1:N Festival -> Stages (FK requerida, cascade)
-            modelBuilder.Entity<Festival>(Festival =>
+            modelBuilder.Entity<Festival>(f =>
             {
-                Festival.HasMany(f => f.Stages ) 
+                f.HasKey(f => f.Id);
+                f.Property(f => f.Name).IsRequired().HasMaxLength(100);
+                f.Property(f => f.City).IsRequired().HasMaxLength(200);
+                f.Property(f => f.StartDate).IsRequired();
+                f.Property(f => f.EndDate).IsRequired();
+                f.HasMany(f => f.Stages ) 
                         .WithOne(s => s.Festival)
                         .HasForeignKey(s => s.FestivalId)
-                        .IsRequired()
                         .OnDelete(DeleteBehavior.Cascade);
-            }); 
-          
+            });
+
 
             // N:M con payload: Performance (clave compuesta)
-          
+            modelBuilder.Entity<Performance>(p =>
+            {
+                p.HasKey(p => new { p.ArtistId, p.StageId });
+                p.Property(p => p.StartTime).IsRequired();
+                p.Property(p => p.EndTime).IsRequired();
+                p.Property(p => p.ArtistId).IsRequired();
+                p.Property(p => p.StageId).IsRequired();
+            }); 
+
             //Índice único: Stage.Name dentro de un Festival
-            
+
         }
     }
 }
--- Models/Festival.cs
+++ Models/Festival.cs
@@ -1,14 +1,17 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TecWebFest.Api.Entities
 {
     public class Festival
     {
-        public int Id { get; set; }
-        public string Name { get; set; } = default!;
-        public string City { get; set; } = default!;
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        [Required] public int Id { get; set; }
+
+        [Required] public string Name { get; set; } = default!;
+
+        [Required] public string City { get; set; } = default!;
+        [Required] public DateTime StartDate { get; set; }
+        [Required] public DateTime EndDate { get; set; }
         //TODO relacion con stage
         public ICollection<Stage> Stages { get; set; } = new List<Stage>(); 
     }
