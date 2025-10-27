6872fb83ad5185889868b531508462cd52f696b4
unknown <nicole.herbas@ucb.edu.bo>
2025-10-27 16:58:02 -0400

    feat: create festival async on service

--- Services/FestivalService.cs
+++ Services/FestivalService.cs
@@ -1,3 +1,4 @@
+using System.Xml;
 using TecWebFest.Api.DTOs;
 using TecWebFest.Api.Entities;
 using TecWebFest.Api.Repositories.Interfaces;
@@ -8,11 +9,22 @@ namespace TecWebFest.Api.Services
     public class FestivalService : IFestivalService
     {
         //TODO INEYECCION DE DEPENDENCIAS
+        private readonly IFestivalRepository _festivals;
+
+        public FestivalService(IFestivalRepository festivals)
+        {
+            _festivals = festivals;
+        }
 
         public async Task<int> CreateFestivalAsync(CreateFestivalDto dto)
         {
             //TODO
             //pista para importar stages: Stages = dto.Stages.Select(s => new Stage { Name = s.Name }).ToList()
+            var entity = new Festival { Name = dto.Name, City = dto.City, StartDate = dto.StartDate, EndDate = dto.EndDate, Stages = dto.Stages.Select(s => new Stage { Name = s.Name }).ToList() };
+            await _festivals.AddAsync(entity);
+            await _festivals.SaveChangesAsync();
+            return entity.Id;
+
         }
 
 
