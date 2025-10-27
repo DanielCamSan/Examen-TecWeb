fff077384641497c9b3067cc3fb0d90614c7e3bd
Fabio Garcia Meza <fabio.garciameza@ucb.edu.bo>
2025-10-27 16:55:58 -0400

    feat(Repositories): Adding injection in stage

--- Repositories/StageRepository.cs
+++ Repositories/StageRepository.cs
@@ -5,7 +5,9 @@ namespace TecWebFest.Repositories
 {
     public class StageRepository : IStageRepository
     {
-        // TODO INYECCION 
+        private readonly AppDbContext _ctx;
+
+        public StageRepository(AppDbContext ctx) => _ctx = ctx;
 
         public Task<bool> ExistsAsync(int id) =>
             _ctx.Stages.AnyAsync(s => s.Id == id);
