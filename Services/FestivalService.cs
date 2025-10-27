b2fb7b882dc32b0996f5ae813e7ffc060c24e626
Flora Ortiz <flora.ortiz@ucb.edu.bo>
2025-10-27 16:57:36 -0400

    feat: FestivalRepository injection

--- Services/FestivalService.cs
+++ Services/FestivalService.cs
@@ -8,6 +8,11 @@ namespace TecWebFest.Api.Services
     public class FestivalService : IFestivalService
     {
         //TODO INEYECCION DE DEPENDENCIAS
+        private readonly IFestivalRepository _repo;
+        public FestivalService(IFestivalRepository repo)
+        {
+            _repo = repo;
+        }
 
         public async Task<int> CreateFestivalAsync(CreateFestivalDto dto)
         {
