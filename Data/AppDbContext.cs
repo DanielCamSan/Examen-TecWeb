83722f2c1cff8ddcdb79a8177f31e77c0b7a2919
Luiscuac <luiguiandre2208@gmail.com>
2025-10-27 16:45:49 -0400

    feat: DbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -15,15 +15,47 @@ namespace TecWebFest.Api.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            //TODO
-
             // 1:N Festival -> Stages (FK requerida, cascade)
-          
+            modelBuilder.Entity<Festival>(entity =>
+            {
+                entity.HasKey(f => f.Id);
+                entity.Property(f => f.Name).IsRequired().HasMaxLength(100);
+                entity.Property(f => f.City).IsRequired().HasMaxLength(100);
+                entity.Property(f => f.StartDate).IsRequired();
+                entity.Property(f => f.EndDate).IsRequired();
+                entity.HasMany(f => f.Stages)
+                      .WithOne(s => s.Festival)
+                      .HasForeignKey(s => s.FestivalId)
+                      .OnDelete(DeleteBehavior.Cascade);
+
+            });
+
+
+         
+         
+
 
             // N:M con payload: Performance (clave compuesta)
-          
+            modelBuilder.Entity<Performance>(entity =>
+            {
+                entity.HasKey(p => new { p.ArtistId, p.StageId});
+                entity.Property(p => p.StartTime).IsRequired();
+                entity.Property(p => p.EndTime).IsRequired();
+                entity.HasOne(p => p.Artist)
+                      .WithMany(a => a.Performances)
+                      .HasForeignKey(p => p.ArtistId)
+                      .OnDelete(DeleteBehavior.Cascade);
+                entity.HasOne(p => p.Stage)
+                      .WithMany(s => s.Performances)
+                      .HasForeignKey(p => p.StageId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+
             //Índice único: Stage.Name dentro de un Festival
-            
+            modelBuilder.Entity<Stage>()
+                .HasIndex(s => new { s.FestivalId, s.Name })
+                .IsUnique();
+
         }
     }
 }
