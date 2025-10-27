5ed42c3ccb2d663f837a9249a65720931233fa67
Flora Ortiz <flora.ortiz@ucb.edu.bo>
2025-10-27 16:42:31 -0400

    feat: adding relation Festival -> Stages

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -15,10 +15,15 @@ namespace TecWebFest.Api.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
             //TODO
 
             // 1:N Festival -> Stages (FK requerida, cascade)
-          
+            modelBuilder.Entity<Festival>()
+                .HasMany(f => f.Stages)
+                .WithOne(s => s.Festival)
+                .HasForeignKey(s => s.FestivalId)
+                .OnDelete(DeleteBehavior.Cascade);
 
             // N:M con payload: Performance (clave compuesta)
           
