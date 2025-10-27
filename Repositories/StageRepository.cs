a5686c3ec7e4460dee1227902492855a66efd303
Maria <mariajesuslozada10@gmail.com>
2025-10-27 17:05:11 -0400

    feat:inyections

--- Repositories/StageRepository.cs
+++ Repositories/StageRepository.cs
@@ -7,6 +7,12 @@ namespace TecWebFest.Repositories
     {
         // TODO INYECCION 
 
+        private readonly AppDbContext _ctx;
+        public StageRepository(AppDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
         public Task<bool> ExistsAsync(int id) =>
             _ctx.Stages.AnyAsync(s => s.Id == id);
     }
