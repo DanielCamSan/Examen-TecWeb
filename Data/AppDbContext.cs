5b38ab57f7e96ca37ef0c9b8ba18587b18cee844
Josue-Camacho <josuecamacho582@gmail.com>
2025-10-27 16:22:48 -0400

    feat: add relations and validation

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -15,15 +15,51 @@ namespace TecWebFest.Api.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            //TODO
 
-            // 1:N Festival -> Stages (FK requerida, cascade)
-          
+            modelBuilder.Entity<Stage>(entity =>
+            {
+                entity.HasKey(s => s.Id);
 
-            // N:M con payload: Performance (clave compuesta)
-          
-            //Índice único: Stage.Name dentro de un Festival
-            
+                entity.HasOne(s => s.Festival)
+                      .WithMany(f => f.Stages)
+                      .HasForeignKey(s => s.FestivalId)
+                      .OnDelete(DeleteBehavior.Cascade)
+                      .IsRequired(); 
+
+                entity.HasIndex(s => new { s.FestivalId, s.Name })
+                      .IsUnique();
+            });
+
+            modelBuilder.Entity<Performance>(entity =>
+            {
+                entity.HasKey(p => new { p.ArtistId, p.StageId, p.StartTime });
+
+                entity.HasOne(p => p.Artist)
+                      .WithMany(a => a.Performances)
+                      .HasForeignKey(p => p.ArtistId)
+                      .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasOne(p => p.Stage)
+                      .WithMany(s => s.Performances)
+                      .HasForeignKey(p => p.StageId)
+                      .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasIndex(p => new { p.StageId, p.StartTime, p.EndTime });
+            });
+
+            modelBuilder.Entity<Festival>(entity =>
+            {
+                entity.HasKey(f => f.Id);
+                entity.Property(f => f.Name).IsRequired().HasMaxLength(200);
+                entity.Property(f => f.City).IsRequired().HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Artist>(entity =>
+            {
+                entity.HasKey(a => a.Id);
+                entity.Property(a => a.StageName).IsRequired().HasMaxLength(200);
+                entity.Property(a => a.Genre).IsRequired().HasMaxLength(100);
+            });
         }
     }
 }
