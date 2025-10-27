b7dafb00a3d46b6a3f41208b65bfb412e05afc2e
Fabio Garcia Meza <fabio.garciameza@ucb.edu.bo>
2025-10-27 17:03:13 -0400

    feat(Program): Adding ijections in program

--- Program.cs
+++ Program.cs
@@ -1,35 +1,35 @@
 using Microsoft.EntityFrameworkCore;
 using TecWebFest.Api.Data;
-
-// Namespaces de repos/servicios
 using TecWebFest.Api.Repositories;
 using TecWebFest.Api.Repositories.Interfaces;
 using TecWebFest.Api.Services;
 using TecWebFest.Api.Services.Interfaces;
-
-// OJO: algunos repos tuyos están en TecWebFest.Repositories
 using TecWebFest.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-//TODO INYECCION DE DEPENDENCIAS
+builder.Services.AddDbContext<AppDbContext>(o => o.UseInMemoryDatabase("TecWebFest"));
+
+builder.Services.AddScoped<IArtistRepository, ArtistRepository>();
+builder.Services.AddScoped<IFestivalRepository, FestivalRepository>();
+builder.Services.AddScoped<IPerformanceRepository, PerformanceRepository>();
+builder.Services.AddScoped<IStageRepository, StageRepository>();
+
+builder.Services.AddScoped<IArtistService, ArtistService>();
+builder.Services.AddScoped<IFestivalService, FestivalService>();
+builder.Services.AddScoped<IPerformanceService, PerformanceService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
