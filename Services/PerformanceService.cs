e929c307824c45c5ff0a7d04441d8d4c5833dd06
unknown <ariana.pita@ucb.edu.bo>
2025-10-27 17:13:15 -0400

    feat: add performance logic

--- Services/PerformanceService.cs
+++ Services/PerformanceService.cs
@@ -25,20 +25,47 @@ namespace TecWebFest.Api.Services
         public async Task AddPerformanceAsync(CreatePerformanceDto dto)
         {
             // PISTA TE PASO COMO EXTRAER START TIME Y END TIME EN FORMATO UTC
-            //var startUtc = dto.StartTime.Kind == DateTimeKind.Unspecified
-            //    ? DateTime.SpecifyKind(dto.StartTime, DateTimeKind.Utc)
-            //    : dto.StartTime.ToUniversalTime();
+            var startUtc = dto.StartTime.Kind == DateTimeKind.Unspecified
+                ? DateTime.SpecifyKind(dto.StartTime, DateTimeKind.Utc)
+                : dto.StartTime.ToUniversalTime();
 
-            //var endUtc = dto.EndTime.Kind == DateTimeKind.Unspecified
-            //    ? DateTime.SpecifyKind(dto.EndTime, DateTimeKind.Utc)
-            //    : dto.EndTime.ToUniversalTime();
+            var endUtc = dto.EndTime.Kind == DateTimeKind.Unspecified
+                ? DateTime.SpecifyKind(dto.EndTime, DateTimeKind.Utc)
+                : dto.EndTime.ToUniversalTime();
 
             // TODO VERIFICAR QUE EXISTA ARTISTA, STAGES Y QUE LA FECHA DE FIN SEA MAYOR QUE LA DEL INICIO
+            if (!await _artists.ExistsAsync(dto.ArtistId))
+            {
+                throw new ArgumentException($"The artist doed not exist.");
+            }
 
+            if (!await _stages.ExistsAsync(dto.StageId))
+            {
+                throw new ArgumentException($"Stage does not exist.");
+            }
+
+            if (endUtc <= startUtc)
+            {
+                throw new ArgumentException("End time must be after start time.");
+            }
 
             // TODO VERIFICAR QUE NO HAYA SOLAPAMIENTO ENTRE PERFEROMANCES VER QUE EL STAGE ESTE LIBRE.
-           
+            if (await _performances.HasOverlapAsync(dto.StageId, startUtc, endUtc))
+            {
+                throw new InvalidOperationException($"The stage is already booked for this time.");
+            }
+
             // ANADIR EL PERFORMANCE
+            var performance = new Performance
+            {
+                ArtistId = dto.ArtistId,
+                StageId = dto.StageId,
+                StartTime = startUtc,
+                EndTime = endUtc
+            };
+
+            await _performances.AddAsync(performance);
+            await _performances.SaveChangesAsync();
         }
     }
 }
